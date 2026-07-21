52370807e454b4eb508d8b49bac8a165284659d5
Vitor Fel <silva.vitor@grupomateus.com>
2026-07-20 22:55:27 -0300

    feat: Cria layout do projeto

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,12 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Layout } from './core/layout/layout';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [Layout, RouterOutlet],
   templateUrl: './app.html',
   styleUrl: './app.scss'
 })
 export class App {
-  protected readonly title = signal('financial-manager');
 }

8a22a6aacf92dee20d5c63cde9baf6465d781c68
Vitor Fel <silva.vitor@grupomateus.com>
2026-07-21 18:02:31 -0300

    fix: Corrige bug do saldo zerado

---
+++ src/app/features/home/components/balance/components/balance-card/balance-card.ts
@@ -0,0 +1,40 @@
+import { Component, computed, input } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+type CardType = 'income' | 'outcome' | 'balance';
+
+enum ValueCssClass {
+  income = 'income',
+  outcome = 'outcome',
+  zero = 'zero',
+}
+
+@Component({
+  selector: 'app-balance-card',
+  imports: [MatCardModule],
+  templateUrl: './balance-card.html',
+  styleUrl: './balance-card.scss',
+})
+export class BalanceCard {
+  type = input.required<CardType>();
+  label = input.required<string>();
+  value = input.required<number>();
+
+  cssClass = computed<ValueCssClass>(() => {
+    const type = this.type();
+    const { income, outcome, zero } = ValueCssClass;
+    if(type === income) {
+      return income;
+    }
+
+    if(type === outcome) {
+      return outcome;
+    }
+
+    if(this.value() === 0) {
+      return zero;
+    }
+
+    return this.value() > 0 ? income : outcome;
+  });
+}

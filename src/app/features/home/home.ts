04ee5a46a32441ac530446a3f160205c951a6e9b
Vitor Fel <silva.vitor@grupomateus.com>
2026-07-21 22:33:10 -0300

    feat: Renderiza uma lista de trasacoes

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -1,16 +1,19 @@
 import { Component, signal } from '@angular/core';
 import { Balance } from './components/balance/balance';
+import { TransactionItem } from "./components/transaction-item/transaction-item";
+import { Transaction } from '../../shared/transaction/interfaces/transaction';
+import { TransactionType } from '../../shared/transaction/enums/transaction-types';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance],
+  imports: [Balance, TransactionItem],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
-  transactions =  signal([
-    { value: 100, type: 'income' },
-    { value: 0, type: 'income' },
-    { value: 100, type: 'outcome' },
+  transactions =  signal<Transaction[]>([
+    { title: 'Salário', value: 100, type: TransactionType.INCOME },
+    { title: 'VA', value: 0, type: TransactionType.INCOME },
+    { title: 'Aluguel', value: 100, type: TransactionType.OUTCOME },
   ]);
 }

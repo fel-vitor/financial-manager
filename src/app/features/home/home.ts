40c81b7f03b377da1646dc003c0669242b96202d
Vitor Fel <silva.vitor@grupomateus.com>
2026-07-21 22:46:41 -0300

    feat: Adiciona componente para mostrar mensagem quando não há transações

--- src/app/features/home/home.ts
+++ src/app/features/home/home.ts
@@ -3,17 +3,18 @@ import { Balance } from './components/balance/balance';
 import { TransactionItem } from "./components/transaction-item/transaction-item";
 import { Transaction } from '../../shared/transaction/interfaces/transaction';
 import { TransactionType } from '../../shared/transaction/enums/transaction-types';
+import { NoTransactions } from './components/no-transactions/no-transactions';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, TransactionItem],
+  imports: [Balance, TransactionItem, NoTransactions],
   templateUrl: './home.html',
   styleUrl: './home.scss',
 })
 export class Home {
   transactions =  signal<Transaction[]>([
-    { title: 'Salário', value: 100, type: TransactionType.INCOME },
-    { title: 'VA', value: 0, type: TransactionType.INCOME },
-    { title: 'Aluguel', value: 100, type: TransactionType.OUTCOME },
+    // { title: 'Salário', value: 100, type: TransactionType.INCOME },
+    // { title: 'VA', value: 0, type: TransactionType.INCOME },
+    // { title: 'Aluguel', value: 100, type: TransactionType.OUTCOME },
   ]);
 }

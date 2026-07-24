4b1e91c3c8af0766fbcd8f0b1905ee97157d4281
Vitor Fel <silva.vitor@grupomateus.com>
2026-07-24 17:06:14 -0300

    feat: Autenticacao

--- src/app/core/auth/pages/login/login.component.ts
+++ src/app/core/auth/pages/login/login.component.ts
@@ -1,7 +1,11 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { MatButtonModule } from '@angular/material/button';
 import { MatInputModule } from '@angular/material/input';
+import { AuthService } from '../../services/auth.service';
+import { UserCredentials } from '../../interfaces/user-credentials';
+import { Router } from '@angular/router';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -10,17 +14,40 @@ import { MatInputModule } from '@angular/material/input';
   styleUrl: './login.component.scss',
 })
 export class LoginComponent {
+  authService = inject(AuthService);
+  router = inject(Router);
 
   form = new FormGroup({
     user: new FormControl('', {
-      validators: [Validators.required]
+      validators: [Validators.required],
     }),
     password: new FormControl('', {
-      validators: [Validators.required]
+      validators: [Validators.required],
     }),
   });
 
   submit() {
-    throw new Error('Method not implemented.');
+    if (this.form.invalid) {
+      return;
+    }
+
+    const payload: UserCredentials = {
+      user: this.form.controls.user.value as string,
+      password: this.form.controls.password.value as string,
+    };
+
+    this.authService.login(payload)
+      .subscribe({
+        next: () => {
+          this.router.navigate(['']);
+        },
+        error: (res: HttpErrorResponse) => {
+          if (res.status === 401) {
+            this.form.setErrors({
+              wrongCredentials: true,
+            });
+          }
+        }
+      });
   }
 }
---
+++ src/app/core/auth/services/auth.service.ts
@@ -0,0 +1,24 @@
+import { HttpErrorResponse } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { Observable, of, throwError } from 'rxjs';
+import { UserCredentials } from '../interfaces/user-credentials';
+import { AuthTokenResponse } from '../interfaces/auth-token-reponse';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class AuthService {
+  login(payload: UserCredentials): Observable<AuthTokenResponse> {
+    if (payload.user === 'admin' && payload.password === '123') {
+      return of({ token: 'fake-token' });
+    }
+
+    return throwError(
+      () =>
+        new HttpErrorResponse({
+          status: 401,
+          statusText: 'Unauthorized',
+        }),
+    );
+  }
+}

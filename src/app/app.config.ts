8d03d74c7bb116d46be2eef73f0ed277e7a89c7c
Vitor Fel <silva.vitor@grupomateus.com>
2026-07-29 15:38:27 -0300

    refactor: Melhora registro de providers com a funcao 'providerCore()'

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -5,31 +5,14 @@ import {
 } from '@angular/core';
 import { provideRouter, withComponentInputBinding } from '@angular/router';
 
-import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig } from '@angular/material/snack-bar';
-import { provideEnvironmentNgxMask } from 'ngx-mask';
 import { routes } from './app.routes';
-import { provideLoggedInUser } from './core/auth/initializers/provide-logged-in-user';
-import { setAuthTokenInterceptor } from './core/auth/interceptors/set-auth-token-interceptor';
+import { provideCore } from './core/provide-core';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes, withComponentInputBinding()),
-    provideHttpClient(withInterceptors([setAuthTokenInterceptor])),
-    provideEnvironmentNgxMask({
-      thousandSeparator: '.',
-      decimalMarker: ',',
-    }),
-    {
-      provide: MAT_SNACK_BAR_DEFAULT_OPTIONS,
-      useValue: {
-        horizontalPosition: 'center',
-        verticalPosition: 'top',
-        duration: 3000,
-      } as MatSnackBarConfig,
-    },
-    provideLoggedInUser(),
+    provideCore(),
   ],
 };

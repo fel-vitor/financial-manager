083bcea092035f7a75b6f470824a52966f495f59
Vitor Fel <silva.vitor@grupomateus.com>
2026-07-29 16:14:26 -0300

    feat: Cria layout para efetuar logout

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,9 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Header } from './components/header/header';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Header],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })

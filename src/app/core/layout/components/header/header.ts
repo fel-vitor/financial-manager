f1d3b971cb7f9f50e41a90c60070be987d22e319
Vitor Fel <silva.vitor@grupomateus.com>
2026-07-29 16:34:53 -0300

    feat: Mostra botao para fazer logou atraves de Signals

--- src/app/core/layout/components/header/header.ts
+++ src/app/core/layout/components/header/header.ts
@@ -1,8 +1,9 @@
-import { Component, inject } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatButtonModule } from '@angular/material/button';
 import { MatToolbarModule } from '@angular/material/toolbar';
 import { LogoutFacadeService } from '../../../auth/facades/logout-facade.service';
 import { Router } from '@angular/router';
+import { LoggedInUserStoreService } from '../../../auth/stores/logged-in-user-store.service';
 
 @Component({
   selector: 'app-header',
@@ -13,12 +14,15 @@ import { Router } from '@angular/router';
 export class Header {
   logoutFacadeService = inject(LogoutFacadeService);
   router = inject(Router);
+  loggedInUserStoreService = inject(LoggedInUserStoreService);
+
+  isLoggedIn = computed(() => this.loggedInUserStoreService.isLoggedIn());
 
   logout() {
     this.logoutFacadeService.logout().subscribe({
       next: () => {
         this.router.navigate(['auth/login']);
-      }
+      },
     });
   }
 }

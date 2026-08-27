8e86c61bd810d8b2210d55219bb76bf0d20350f6
Vitor Fel <silva.vitor@grupomateus.com>
2026-08-27 11:00:58 -0300

    feat: Configurando CurrencyPipe

--- src/app/core/provide-core.ts
+++ src/app/core/provide-core.ts
@@ -1,9 +1,13 @@
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
-import { makeEnvironmentProviders } from '@angular/core';
+import { DEFAULT_CURRENCY_CODE, LOCALE_ID, makeEnvironmentProviders } from '@angular/core';
 import { MAT_SNACK_BAR_DEFAULT_OPTIONS, MatSnackBarConfig } from '@angular/material/snack-bar';
 import { provideEnvironmentNgxMask } from 'ngx-mask';
 import { setAuthTokenInterceptor } from './auth/interceptors/set-auth-token-interceptor';
 import { provideAuth } from './auth/provider-auth';
+import { registerLocaleData } from '@angular/common';
+import ptBr from '@angular/common/locales/pt';
+
+registerLocaleData(ptBr);
 
 export function provideCore() {
   return makeEnvironmentProviders([
@@ -20,6 +24,14 @@ export function provideCore() {
         verticalPosition: 'top',
         duration: 3000,
       } as MatSnackBarConfig,
+    },
+    {
+      provide: DEFAULT_CURRENCY_CODE,
+      useValue: 'BRL',
+    },
+    {
+      provide: LOCALE_ID,
+      useValue: 'pt',
     },
   ]);
 }
--- src/app/features/home/pages/list/components/transaction-item/components/transaction-value/transaction-value.ts
+++ src/app/features/home/pages/list/components/transaction-item/components/transaction-value/transaction-value.ts
@@ -1,3 +1,4 @@
+import { CurrencyPipe } from '@angular/common';
 import { Component, computed, input } from '@angular/core';
 import { TransactionType } from '@shared/transaction/enums/transaction-types';
 import { Transaction } from '@shared/transaction/interfaces/transaction';
@@ -9,8 +10,8 @@ const CssClasses = {
 
 @Component({
   selector: 'app-transaction-value',
-  imports: [],
-  template: '{{ transaction().value }}',
+  imports: [CurrencyPipe],
+  template: `{{ transaction().value | currency }}`,
   styleUrl: './transaction-value.scss',
   host: {
     '[class]': 'cssClass()',

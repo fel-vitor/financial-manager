9f0ce95e0b6ee055721976fc2454508183045ba4
Vitor Fel <silva.vitor@grupomateus.com>
2026-08-28 10:47:27 -0300

    feat: Implementando mat-nav-list

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
+import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Header],
+  imports: [RouterOutlet, Header, MatSidenavModule, SidenavItemsComponent],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })
-export class Layout {
-
-}
+export class Layout {}

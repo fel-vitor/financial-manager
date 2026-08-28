902bfda4a79df8792c2a228c434311c04df6f162
Vitor Fel <silva.vitor@grupomateus.com>
2026-08-28 17:19:06 -0300

    feat: Criando componente para abstrair sidenav

--- src/app/core/layout/layout.ts
+++ src/app/core/layout/layout.ts
@@ -1,12 +1,11 @@
 import { Component } from '@angular/core';
-import { MatSidenavModule } from '@angular/material/sidenav';
 import { RouterOutlet } from '@angular/router';
 import { Header } from './components/header/header';
-import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
+import { SidenavComponent } from './components/sidenav/sidenav.component';
 
 @Component({
   selector: 'app-layout',
-  imports: [RouterOutlet, Header, MatSidenavModule, SidenavItemsComponent],
+  imports: [RouterOutlet, Header, SidenavComponent],
   templateUrl: './layout.html',
   styleUrl: './layout.scss',
 })

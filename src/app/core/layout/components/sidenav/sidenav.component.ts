4856371d121111b73eb2de2de300a2f85d8a6a1f
Vitor Fel <silva.vitor@grupomateus.com>
2026-08-28 17:37:12 -0300

    feat: Mudando como siidenav aparece conforme media-qquery

--- src/app/core/layout/components/sidenav/sidenav.component.ts
+++ src/app/core/layout/components/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, computed, inject } from '@angular/core';
 import { MatSidenavModule } from '@angular/material/sidenav';
+import { MobileLayoutService } from '@core/layout/services/mobile-layout.service';
 import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.component';
 
 @Component({
@@ -9,5 +10,9 @@ import { SidenavItemsComponent } from './components/sidenav-items/sidenav-items.
   styleUrl: './sidenav.component.scss',
 })
 export class SidenavComponent {
+  private readonly mobileLayoutService = inject(MobileLayoutService);
 
+  isMobile = this.mobileLayoutService.isMobile();
+
+  sideNavMode = computed(() => (this.isMobile() ? 'over' : 'side'));
 }

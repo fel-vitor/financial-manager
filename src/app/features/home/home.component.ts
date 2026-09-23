71f7a681053bee4409078f2385b54c67529f2982
Vitor Fel <silva.vitor@grupomateus.com>
2026-09-23 08:50:35 -0300

    feat: Melhorando estilização da renderização do gráfico

--- src/app/features/home/home.component.ts
+++ src/app/features/home/home.component.ts
@@ -1,19 +1,25 @@
-import { Component, computed, input, linkedSignal } from '@angular/core';
-import { Transaction } from '@shared/transaction/interfaces/transaction';
-import { Balance } from './components/balance/balance';
-import { PieChartComponent } from './components/pie-chart/pie-chart.component';
-import { PieChartConfig } from './components/pie-chart/pie-chart-config.interface';
+import { Component, computed, input, signal } from '@angular/core';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
 import { TransactionType } from '@shared/transaction/enums/transaction-types';
 import { sumTransaction } from '@shared/transaction/functions/sum-transactions';
+import { Transaction } from '@shared/transaction/interfaces/transaction';
+import { Balance } from './components/balance/balance';
+import { PieChartConfig } from './components/pie-chart/pie-chart-config.interface';
+import { PieChartComponent } from './components/pie-chart/pie-chart.component';
+import { MatProgressBarModule } from '@angular/material/progress-bar';
+import { MatIcon, MatIconModule } from '@angular/material/icon';
 
 @Component({
   selector: 'app-home',
-  imports: [Balance, PieChartComponent],
+  imports: [Balance, PieChartComponent, MatCardModule, MatButtonModule, MatProgressBarModule, MatIconModule],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
 })
 export class HomeComponent {
   transactions = input.required<Transaction[]>();
+
+  canLoadComponent = signal(false);
 
   totalIncomes = computed(() => {
     return sumTransaction(this.transactions(), TransactionType.INCOME);
